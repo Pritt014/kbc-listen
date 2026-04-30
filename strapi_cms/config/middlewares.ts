67a303d64e0e5c4f9c6e165964b45c679d86c558
Pritt014 <pritt.drummer@gmail.com>
2026-04-30 09:36:57 -0400

    Allow Netlify origin in Strapi CORS

--- strapi_cms/config/middlewares.ts
+++ strapi_cms/config/middlewares.ts
@@ -1,23 +1,18 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: false,
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: [
+      origin: env.array('CORS_ORIGIN', [
+        'https://kbc-listen.netlify.app',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://localhost:1337',
-      ],
+      ]),
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: '*',
-      methods: '*',
       credentials: true,
       expose: '*',
     },
